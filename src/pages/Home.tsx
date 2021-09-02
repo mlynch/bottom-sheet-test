8e598f3a664717a4eb449bf61fc55b1799719e1b
Max Lynch <max@ionic.io>
2021-09-02 11:37:49 -0500

    First

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,11 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonModal, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { useState } from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
+  const [showModal, setShowModal] = useState(false);  
+
   return (
     <IonPage>
       <IonHeader>
@@ -16,9 +19,86 @@ const Home: React.FC = () => {
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        <Modal isOpen={showModal} onDidDismiss={() => setShowModal(false)} />
+        <IonButton onClick={() => setShowModal(true)}>Open Modal</IonButton>
       </IonContent>
     </IonPage>
+  );
+};
+
+const Modal: React.FC<any> = ({ isOpen, onDidDismiss }: any) => {
+  return (
+    <IonModal isOpen={isOpen} initialBreakpoint={0.5} breakpoints={[0, 0.5, 1]} onDidDismiss={onDidDismiss}>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Modal</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+        This is modal content
+      </IonContent>
+    </IonModal>
   );
 };
 
